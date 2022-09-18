b3486153774f7880d5fb6f45558c89a758cff48f
gastondonosowo <75701041+gastondonosowo@users.noreply.github.com>
2022-09-18 14:57:49 -0300

    Delete OneDrive directory

--- OneDrive/Documents/UDP/Sistemas Distribuidos/T1/WSE/backend/config/proto.sh
+++
@@ -1,2 +0,0 @@
-cd /backend/config
-python -m grpc_tools.protoc -I config --python_out=. --grpc_python_out=. config/search.proto
--- OneDrive/Documents/UDP/Sistemas Distribuidos/T1/WSE/db/noInit.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE T1(id SERIAL , title VARCHAR(255), description VARCHAR(255), keywords VARCHAR(255), web VARCHAR(100));
-INSERT INTO T1 (title, description, keywords, web) VALUES('afdsafsd', 'sdgsd asdg asd gsd ', 'asdg adgs asdasf asd', 'asdfsdf sdf sdf sdfa ');
-INSERT INTO T1 (title, description, keywords, web) VALUES( 'fffffffffffffff', 'ffffffffffffffff', 'fffffffffffffffffffffff', 'ffffffffffffffffffff');
-INSERT INTO T1 (title, description, keywords, web) VALUES( 'aaaaaaaaaaaaa', 'aaaaaaaaaaaaa', 'aaaaaaaaaaaaa', 'aaaaaaaaaaaaa');
-INSERT INTO T1 (title, description, keywords, web) VALUES( 'bbbbbbbbbbbbbb', 'bbbbbbbbbbbbbb', 'bbbbbbbbbbbbbb', 'bbbbbbbbbbbbbb');
-INSERT INTO T1 (title, description, keywords, web) VALUES( 'cccccccccccc', 'cccccccccccc', 'cccccccccccc', 'cccccccccccc');
-
